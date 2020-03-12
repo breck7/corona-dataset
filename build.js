4401027e08cb18f008eebf14e2bd1a06ffe16bc1
Breck Yunits <breck7@gmail.com>
2020-03-11 16:33:36 -1000

    Standardize on OWID country names

--- build.js
+++ build.js
@@ -4,7 +4,9 @@ const { Disk } = require("jtree/products/Disk.node.js")
 const { jtree } = require("jtree")
 const moment = require("moment")
 const path = __dirname + "/full_data.csv"
+const regionsPath = __dirname + "/regions.csv"
 const data = jtree.TreeNode.fromCsv(Disk.read(path).trim())
+const regions = jtree.TreeNode.fromCsv(Disk.read(regionsPath).trim())
 
 const zeroDay = "2020-01-21"
 const dateFormat = "YYYY-MM-DD"
@@ -14,11 +16,16 @@ const convertDateToDayIncrement = date =>
     .diff(moment(zeroDay, "YYYY-MM-DD"), "days")
     .toString()
 
+const renameToOwidCountryName = whoName => {
+  const match = regions.where("WHOCountryName", "=", whoName).nodeAt(0)
+  return match ? match.get("OWIDCountryName") : whoName
+}
+
 data.forEach(node => {
   node.set("year", convertDateToDayIncrement(node.get("date")))
   node.rename("location", "country")
+  node.set("country", renameToOwidCountryName(node.get("country")))
   node.delete("date")
 })
 
 Disk.write("output.csv", data.toDelimited(",", "country,year,new_cases,new_deaths,total_cases,total_deaths".split(",")))
-
--- fetch.sh
+++ fetch.sh
@@ -1,1 +1,2 @@
 wget -O "full_data.csv" https://raw.githubusercontent.com/owid/covid-19-who/master/data/full_data.csv
+wget -O "regions.csv" https://raw.githubusercontent.com/owid/covid-19-who/master/data/regions.csv
